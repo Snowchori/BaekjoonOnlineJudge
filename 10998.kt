4c5342b6d8a20323be40f6ad95e56152a511de0b
Snowchori <59251108+Snowchori@users.noreply.github.com>
2021-06-27 17:12:27 +0900

    A×B

---
+++ 10998.kt
@@ -0,0 +1,25 @@
+/*
+10998번
+
+문제
+두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A×B를 출력한다.
+ */
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    // a, b 입력받음
+    val (a: Int, b: Int) = readLine()
+        .split(" ")
+        .map{it.toInt()}
+
+    // a * b 출력
+    println(a * b)
+}
